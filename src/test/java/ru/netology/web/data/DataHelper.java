f45a77da86f3aa6766992b0a38aa45475208288d
Galina <gana88tit@gmail.com>
2024-02-26 16:42:24 +0300

    сhange project

--- src/test/java/ru/netology/web/data/DataHelper.java
+++ src/test/java/ru/netology/web/data/DataHelper.java
@@ -28,7 +28,7 @@ public class DataHelper {
 
     public static int generateValidAmount(int balance) {
 
-        return new Random().nextInt(Math.abs(balance)) + 1;
+        return new Random().nextInt(10000);
     }
 
     public static int generateInvalidAmount(int balance) {
